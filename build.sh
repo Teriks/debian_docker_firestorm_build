9a432e67960ae616e6af9c64d6376ae0983f8fa6
Teriks <Teriks@users.noreply.github.com>
2017-06-06 08:45:26 -0500

    Add missing . at the end of docker build command

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ IMAGE_VERSION=0.1.0
 
 if [[ "$(docker images -q firestorm_build_env_ubuntu_16.04:$IMAGE_VERSION 2> /dev/null)" == "" ]]
 then
-	docker build --tag firestorm_build_env_ubuntu_16.04:$IMAGE_VERSION
+	docker build --tag firestorm_build_env_ubuntu_16.04:$IMAGE_VERSION .
 fi
 
 docker run -v ${PWD}:/home/fs_build firestorm_build_env_ubuntu_16.04:$IMAGE_VERSION \
