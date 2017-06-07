13a98c342960d42ce89783c78333fe344863860a
Teriks <Teriks@users.noreply.github.com>
2017-06-07 07:18:58 -0500

    Map autobuild install.cache to a local directory

--- build.sh
+++ build.sh
@@ -20,17 +20,22 @@ case "$(uname -s)" in
        ;;
 esac
 
+mkdir -p install.cache
+
 if [ -n "${WIN_VOLUME+set}" ]
 then
     docker run \
     -e ON_WINDOWS=true \
-    -v $WIN_VOLUME:/home/fs_build $IMAGE:$IMAGE_VERSION src/entry.sh
+    -v "$WIN_VOLUME":/home/fs_build \
+    -v "$WIN_VOLUME\\install.cache":/var/tmp/root/install.cache \
+    $IMAGE:$IMAGE_VERSION src/entry.sh
 else
     docker run \
     -e ON_WINDOWS=false \
     -e LOCAL_USER_ID=`id -u $USER` \
     -e LOCAL_USER="$USER" \
-    -v $PWD:/home/fs_build $IMAGE:$IMAGE_VERSION src/entry.sh
+    -v "$PWD/install.cache":"/var/tmp/$USER/install.cache" \
+    -v "$PWD":/home/fs_build $IMAGE:$IMAGE_VERSION src/entry.sh
 fi
 
 popd
