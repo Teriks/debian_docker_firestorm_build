143fd3b802cf9c2ce2e7334ce80907254af74a5d
Teriks <Teriks@users.noreply.github.com>
2017-06-08 05:51:33 -0500

    Make sure interactive shell runs under right user

--- src/entry.sh
+++ src/entry.sh
@@ -2,16 +2,16 @@
 
 export HOME=/home/build
 
-if [ "$INTERACTIVE" = true ]
-then
+
+function do_interactive_shell_message()
+{
     echo "=========================================="
     echo "Dropping into interactive container shell."
     echo "Current directory is: \"$PWD\"".
     echo "Leave with 'exit'."
     echo "=========================================="
     echo ""
-    exec /bin/bash
-fi
+}
 
 
 if [ "$ON_WINDOWS" = true ]
@@ -25,15 +25,28 @@ then
     # Make the install.cache local to the volume by linking it, so it can be messed with from the interactive shell on windows and be persistant.
     ln -s "$HOME/install.cache" /var/tmp/root/install.cache
     
+    if [ "$INTERACTIVE" = true ]
+    then
+        # Drop into a shell if the user requested to 
+        do_interactive_shell_message
+        exec /bin/bash
+    fi
+    
     # Windows does not care about volume permissions, just run the build
     exec "$HOME/src/build_in_docker.sh"
 else
-    
     USER_ID=${LOCAL_USER_ID:-9001}
     USER_NAME=${LOCAL_USER:-build_user}
     
     # /home/fs_build exists as a mounted volume, useradd warns that it exists but it can be ignored
     useradd --shell /bin/bash -d "$HOME" -u $USER_ID -o -c "" -m "$USER_NAME" > /dev/null 2>&1
+    
+    if [ "$INTERACTIVE" = true ]
+    then
+        # Drop into a shell if the user requested to 
+        do_interactive_shell_message
+        exec /usr/local/bin/gosu "$USER_NAME" /bin/bash
+    fi
 
     exec /usr/local/bin/gosu "$USER_NAME" bash "$HOME/src/build_in_docker.sh"
 fi
